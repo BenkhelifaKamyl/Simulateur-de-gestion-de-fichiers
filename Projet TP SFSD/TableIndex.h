a58fc235115323b0d33e882e484a4a3e07178538
BenkhelifaKamyl <155434167+BenkhelifaKamyl@users.noreply.github.com>
2024-12-23 16:38:18 +0100

    Update TableIndex.h

--- Projet TP SFSD/TableIndex.h
+++ Projet TP SFSD/TableIndex.h
@@ -12,24 +12,24 @@ typedef struct{
     int numBloc;
 } Index; //Verifier si cette structure est correcte pour les 2 cas dense et non dense, faire si necessaire une autre structure
 
-Index TableIndex[MAX_BLOCKS];
+
 FILE *tablesIndex; //Fichier contenant tous les fichiers d'index
-//Les parametres des fonctions n'ont pas encore été attribuées
+//Les parametres des fonctions n'ont pas encore Ã©tÃ© attribuÃ©es
 
 //Mise a jour de l'etat des blocs apres chaque mise a jour
 void TableAllocation();
 
 //Creation de la table d'index
-void creationTableIndexDense();
-void creationTableIndexNonDense();
+void creationTableIndexDense(fichier F, Index densetableIndex []);
+void creationTableIndexNonDense(fichier F, Index tableIndex []);
 //Sauvegarder la table d'index en memoire secondaire
-void sauvegardeTableIndex();
+void sauvegardeTableIndex(fichier *F, Index tableindex[]);
 //Mettre le fichier d'index dans une table d'index en Memoire centrale
 void chargementFichierIndexDense();
 void chargementFichierIndexNonDense();
 void rechercheEnregistrementDense();
 void rechercheEnregistrementNonDense();
-//Cas de mise à jour, suppression, insertion et changement d'adresse  (Peut etre separe en plusieurs fonctions mais est preferable de le faire en une seule)
+//Cas de mise Ã  jour, suppression, insertion et changement d'adresse  (Peut etre separe en plusieurs fonctions mais est preferable de le faire en une seule)
 void MajTableIndexDense();
 void MajTableIndexNonDense();
 void clearIndexTable(fichier *F); // Update the index table: clear the entries, as all blocks are free
